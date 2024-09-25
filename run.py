28d8c5c1de0c6b52c0e95187cb4515bff30791e7
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2024-09-25 16:51:48 +0000

    wire up the API and the functions to enter the sales data

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials 
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+#sales = SHEET.worksheet('sales')
+#data = sales.get_all_values()
+#print(data)
+
+def get_sales_values():
+    """
+    Get the input from the user
+    """
+    print("Please enter the sales from the last market.")
+    print("Datas should be six numbers separated by commas.")
+    print("Examples: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here")
+    print(f"The data provided is {data_str}")
+
+get_sales_values()    
+
+
